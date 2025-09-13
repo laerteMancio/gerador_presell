151687932c6ab1e831d9f7ce173e4ec5a57ac616
berserker1984 <laerte.mancio@gmail.com>
2025-09-13 05:34:54 -0300

    deploy server.js na vercel2

--- api/server.js
+++ api/server.js
@@ -5,16 +5,16 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 
 // Rotas
-const generateAuth = require("./routes/generate-auth");
-const links = require("./routes/links");
+//const generateAuth = require("./routes/generate-auth");
+//const links = require("./routes/links");
 const login = require("./routes/login");
-const registerPublic = require("./routes/register-public");
-const usuariosRoutes = require("./routes/usuarios");
-const publishPresellRouter = require("./routes/publishPresell");
-const deployRoute = require("./routes/deploy");
-const updateSubdomainStatus = require("./routes/updateSubdomainStatus");
-const getUserProjects = require("./routes/getUserProjects");
-const vercelCheckDomain = require("./routes/vercelCheck"); 
+//const registerPublic = require("./routes/register-public");
+//const usuariosRoutes = require("./routes/usuarios");
+//const publishPresellRouter = require("./routes/publishPresell");
+//const deployRoute = require("./routes/deploy");
+//const updateSubdomainStatus = require("./routes/updateSubdomainStatus");
+//const getUserProjects = require("./routes/getUserProjects");
+//const vercelCheckDomain = require("./routes/vercelCheck"); 
 
 dotenv.config();
 
@@ -56,17 +56,17 @@ app.use(cookieParser());
 app.use(express.json());
 
 // ------------------ Rotas ------------------
-app.use("/generate-auth", generateAuth);
-app.use("/links", links);
+//app.use("/generate-auth", generateAuth);
+//app.use("/links", links);
 app.use("/login", login);
-app.use("/register-public", registerPublic);
-app.use("/registerPublic", registerPublic);
-app.use("/usuarios", usuariosRoutes);
-app.use("/publicar-presell", publishPresellRouter);
-app.use("/vercel", deployRoute);
-app.use("/subdomain", updateSubdomainStatus);
-app.use("/projects", getUserProjects);
-app.use("/check-subdomain", vercelCheckDomain); 
+//app.use("/register-public", registerPublic);
+//app.use("/registerPublic", registerPublic);
+//app.use("/usuarios", usuariosRoutes);
+//app.use("/publicar-presell", publishPresellRouter);
+//app.use("/vercel", deployRoute);
+//app.use("/subdomain", updateSubdomainStatus);
+//app.use("/projects", getUserProjects);
+//app.use("/check-subdomain", vercelCheckDomain); 
 
 // ------------------ Export ------------------
 module.exports = app;
